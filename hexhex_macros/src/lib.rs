cb7ffb71ba823842543d03fb7861d080c17b38fc
Lukas Himbert <lukas@2.71828.eu>
2023-09-28 22:59:59 +0200

    replace panics in hex_literal! with compile_error!

--- hexhex_macros/src/lib.rs
+++ hexhex_macros/src/lib.rs
@@ -3,6 +3,12 @@
 use hexhex_impl::decode;
 use proc_macro::{Literal, TokenStream, TokenTree};
 
+macro_rules! return_compile_error {
+    ($($fmt:tt)*) => {{
+        return compile_error(&format!($($fmt)*));
+    }}
+}
+
 /// Convert a hex literal (string or byte string) to bytes at compile time.
 ///
 /// # Examples
@@ -30,22 +36,45 @@ pub fn hex_literal(input: TokenStream) -> TokenStream {
             } else if s.starts_with("b\"") && s.ends_with('"') {
                 &s[2..s.len() - 1]
             } else {
-                panic!("unexpected argument {}", lit);
+                return_compile_error!("unexpected argument {lit}");
             };
 
             let bytes = match decode(content) {
                 Ok(x) => x,
-                Err(e) => panic!("{e}"),
+                Err(e) => return_compile_error!("{e}"),
             };
 
             if let Some(tree) = iter.next() {
-                panic!("unexpected argument {}", tree);
+                return_compile_error!("unexpected argument {tree}");
             }
             [TokenTree::from(Literal::byte_string(&bytes))]
                 .into_iter()
                 .collect()
         }
-        None => panic!("expected a string or bytestring"),
-        _ => panic!("unexpected argument"),
+        None => return_compile_error!("expected a string or bytestring"),
+        _ => return_compile_error!("unexpected argument"),
     }
+}
+
+/// Return a token tree that causes a compile error with a nice error message
+fn compile_error(msg: &str) -> TokenStream {
+    // https://stackoverflow.com/a/75506477
+    use proc_macro::{Delimiter, Group, Ident, Punct, Spacing, Span};
+    [
+        TokenTree::Punct(Punct::new(':', Spacing::Joint)),
+        TokenTree::Punct(Punct::new(':', Spacing::Joint)),
+        TokenTree::Ident(Ident::new("std", Span::mixed_site())),
+        TokenTree::Punct(Punct::new(':', Spacing::Joint)),
+        TokenTree::Punct(Punct::new(':', Spacing::Joint)),
+        TokenTree::Ident(Ident::new("compile_error", Span::mixed_site())),
+        TokenTree::Punct(Punct::new('!', Spacing::Alone)),
+        TokenTree::Group(Group::new(
+            Delimiter::Parenthesis,
+            [TokenTree::Literal(Literal::string(msg))]
+                .into_iter()
+                .collect(),
+        )),
+    ]
+    .into_iter()
+    .collect()
 }
